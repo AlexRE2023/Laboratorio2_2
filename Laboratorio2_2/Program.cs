aac7e8b9e9933c53e4121e86c634773578352ddc
alexi <alexi@ALEXANDER>
2023-08-27 21:59:23 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio2_2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Ingrese el precio consumido:");
+int cons = int.Parse(Console.ReadLine());
+
+if (cons <= 100)
+{
+    double descuento = 0.10 * cons;
+    Console.WriteLine("Descuento: " + descuento);
+
+    double subtotal = cons - descuento;
+    Console.WriteLine("Subtotal: " + subtotal);
+
+    double impuesto = 0.18 * subtotal;
+    Console.WriteLine("Impuesto: " + impuesto);
+
+    double Preciototal = subtotal + impuesto;
+    Console.WriteLine("Cantidad a pagar: " + Preciototal);
+}
+else if (cons >= 100)
+{
+    double descuento = 0.20 * cons;
+    Console.WriteLine("Descuento: " + descuento);
+
+    double subtotal = cons - descuento;
+    Console.WriteLine("Subtotal: " + subtotal);
+
+    double impuesto = 0.18 * subtotal;
+    Console.WriteLine("Impuesto: " + impuesto);
+
+    double Preciototal = subtotal + impuesto;
+    Console.WriteLine("Cantidad a pagar: " + Preciototal);
+}
